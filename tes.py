3ede0fed61a5eb485d816b7fe8d022719c9e0cbe
nathanform <127980511+nathanform@users.noreply.github.com>
2023-11-11 09:48:14 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+
 
 # Menu utama
 menu_utama = st.sidebar.radio("Menu Utama", ["Beranda", "Data Grafik"])
